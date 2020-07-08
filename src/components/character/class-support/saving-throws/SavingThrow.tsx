cfe2582230fb1ce7d3a3542ffc76e923b6d60e76
ryguy806 <L1ghtSab3r11!!>
2020-07-08 16:29:39 -0700

    Created saving throws.

---
+++ src/components/character/class-support/saving-throws/SavingThrow.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Stat from '../../information/stat-block/Stat';
+
+export default class SavingThrow extends Component {
+
+    render(): JSX.Element {
+
+        return (
+            <div>
+                <Stat>Saving Throw goes here...</Stat>
+            </div>
+        );
+    }
+}
---
+++ src/components/character/class-support/saving-throws/SavingThrows.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class SavingThrows extends Component {
+
+    render(): JSX.Element {
+
+        return (
+            <div className="saving-throws">
+                <ul>
+                    <li>Saving Throws go here...</li>
+                </ul>
+            </div>
+        );
+    }
+}
